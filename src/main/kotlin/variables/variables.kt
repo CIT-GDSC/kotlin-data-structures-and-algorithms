ba69777db566660a1ec75e50617fd4077d311ed7
Domains18 <gibsonsgibson88@gmail.com>
2023-05-14 19:33:52 +0300

    basic data types
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- src/main/kotlin/variables/variables.kt
+++ src/main/kotlin/variables/variables.kt
@@ -7,7 +7,7 @@ fun main() {
 
 fun callUserName() {
 	val userName: String = "Alpha user"
-	val amount: Number = 18
+	val amount: Int = 18
 	val condition: Boolean = true
 	print("welcome to kotlin, $userName your Number is $amount and the condition is $condition")
 }
