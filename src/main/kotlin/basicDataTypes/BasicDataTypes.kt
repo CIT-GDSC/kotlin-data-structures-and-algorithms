3bfc9cea8fb53c6fa9f9af2d4bfe9fcef161d3c5
Domains18 <gibsonsgibson88@gmail.com>
2023-05-15 09:41:12 +0300

    kotlin data types

--- src/main/kotlin/basicDataTypes/BasicDataTypes.kt
+++ src/main/kotlin/basicDataTypes/BasicDataTypes.kt
@@ -1,1 +1,12 @@
 package basicDataTypes
+
+
+fun main() {
+	//Kotlin hasNumeric Data types such as floats and can be declared as follows:
+	val exampleFloat = 0.8F
+	val floatExample = 2.8
+
+	//long is a 64-bit signed integer, It has a range of -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807. Long is the default type for integers that are too large to be represented by the Int type.
+	val longInt: Long = 12458
+	val anotherLong = 1245L
+}
